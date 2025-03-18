d1d553ad53de0ae1897d0b5d2572fe30971dc6ba
yosefyeshua <yosefyeshua4@gmail.com>
2025-03-18 22:32:52 +0200

    Initial commit

---
+++ src/Employee.java
@@ -0,0 +1,43 @@
+public abstract class Employee {
+    private String firstName;
+    private String lastName;
+    private int id;
+
+    public Employee(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
+    public Employee(){
+        this.firstName = "Plony";
+        this.lastName = "Almony";
+        this.id = 0;
+    }
+    // Getters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // Setters
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+}
